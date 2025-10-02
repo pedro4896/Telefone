8e814c4e4220e40cf8199509a072530d4ff257cb
Pedro Victor da Silva Pereira <pedrovictordasilvapereira4896@gmail.com>
2025-10-02 12:13:57 -0300

    ajustar KeyPad para enviar dígitos ao modelo

--- src/KeyPad.java
+++ src/KeyPad.java
@@ -1,10 +1,8 @@
 package src;
 
-import java.util.Random;
-
 /**
- * Mimic the data input ability of a physical phone's keypad;
- * however, here we're just sending it fake digits.
+ * Simula o teclado do telefone.
+ * Pressionar uma tecla envia o dígito ao modelo.
  */
 public class KeyPad {
     private final PhoneModel model;
@@ -13,14 +11,8 @@ public class KeyPad {
         this.model = model;
     }
 
-    public void simulateKeyPresses(int numKeyPresses) {
-        final int MAX_DIGIT = 12;
-        Random rnd = new Random();
-        for (int i = 0; i < numKeyPresses; i++) {
-            int newDigit = rnd.nextInt(MAX_DIGIT);
-            System.out.println("Pressing: " + newDigit);
-            model.addDigit(newDigit);
-        }
+    public void press(String digit) {
+        System.out.println("Pressionando: " + digit);
+        model.insertDigit(digit);
     }
-
 }
